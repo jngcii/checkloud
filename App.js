97a5e2a02068256e0997a7d72b5a0c4a336d4967
jngcii <concotree@gmail.com>
2019-08-07 23:28:14 +0900

    Provide apollo-client and Theme to app

--- App.js
+++ App.js
@@ -1,4 +1,17 @@
 import React from "react";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
+import { ThemeProvider } from "styled-components";
+import Theme from "./Styles/Theme";
+import client from "./API/apolloClient";
 import AppContainer from "./Components/AppContainer";
 
-export default () => <AppContainer />;
+export default () => (
+	<ApolloProvider client={client}>
+		<ApolloHooksProvider client={client}>
+			<ThemeProvider theme={Theme}>
+				<AppContainer />
+			</ThemeProvider>
+		</ApolloHooksProvider>
+	</ApolloProvider>
+);
