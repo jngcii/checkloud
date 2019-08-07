1b91bdddf6162e95f37f9025adccb109bfa11e8a
jngcii <concotree@gmail.com>
2019-08-07 23:11:24 +0900

    Create apollo-client and localState(typeDefs, resolvers)

---
+++ API/apolloClient.js
@@ -0,0 +1,89 @@
+import AsyncStorage from "@react-native-community/async-storage";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { resolvers, typeDefs } from "./localState";
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+	cache,
+	typeDefs,
+	resolvers
+});
+
+// AsyncStorage.clear();
+
+AsyncStorage.getItem("histories", (_, result) => {
+	if (result) {
+		cache.writeData({
+			data: { histories: JSON.parse(result) }
+		});
+	} else {
+		cache.writeData({
+			data: { histories: [] }
+		});
+	}
+});
+
+AsyncStorage.getItem("plans", (_, result) => {
+	if (result) {
+		cache.writeData({
+			data: { plans: JSON.parse(result) }
+		});
+	} else {
+		cache.writeData({
+			data: {
+				plans: []
+			}
+		});
+	}
+});
+
+AsyncStorage.getItem("items", (_, result) => {
+	if (result) {
+		cache.writeData({
+			data: { items: JSON.parse(result) }
+		});
+	} else {
+		cache.writeData({
+			data: {
+				items: [
+					{
+						__typename: "Item",
+						id: "a",
+						keyword: "Category",
+						color: "#000",
+						parentId: null,
+						childIds: []
+					}
+				]
+			}
+		});
+	}
+});
+
+AsyncStorage.getItem("itemSgts", (_, result) => {
+	if (result) {
+		cache.writeData({
+			data: { itemSgts: JSON.parse(result) }
+		});
+	} else {
+		cache.writeData({
+			data: { itemSgts: [] }
+		});
+	}
+});
+
+AsyncStorage.getItem("itemActs", (_, result) => {
+	if (result) {
+		cache.writeData({
+			data: { itemActs: JSON.parse(result) }
+		});
+	} else {
+		cache.writeData({
+			data: { itemActs: [] }
+		});
+	}
+});
+
+export default client;
---
+++ API/localState.js
@@ -0,0 +1,65 @@
+export const typeDefs = `
+    type Query {
+        histories: [History]
+        
+        plans: [Plan]
+		plan(id: String!): Plan
+        items: [Item]
+        item(id: String!): Item
+        itemSgts: [ItemSgt]
+        itemActs: [ItemAct]		
+        
+        parentKeywords(id: String!): [String]
+    }
+
+    type Mutation {}
+
+	type History {
+		id: String!
+		year: Int!
+		month: Int!
+		date: Int!
+		day: Int!
+		plans: [Plan]
+	}
+
+    type Plan {
+        id: String!
+		title: String!
+		startAt: [Int]
+		repeat: [String]
+		itemActs: [ItemAct]
+		isUsed: Boolean!
+        isActive: Boolean!
+        isDone: Boolean!
+		isMain: Boolean!
+	}
+
+    type Item {
+        id: String!
+        keyword: String!
+        color: String!
+        parentId: String
+        childIds: [String]
+    }
+    type ItemSgt {
+        id: String!
+        keyword: String!
+        color: String!
+    }
+    type ItemAct {
+        id: String!
+        keyword: String!
+        color: String!
+        isChecked: Boolean!
+		parentId: String
+		childKeywords: [String]
+		finishedTime: [Int]
+		memo: String
+    }
+`;
+
+export const resolvers = {
+	Query: {},
+	Mutation: {}
+};
