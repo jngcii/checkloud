968e578bb65f1387e9dbb8e1a5a47392c466cd5d
jngcii <concotree@gmail.com>
2019-08-08 20:25:05 +0900

    Make frame of Swiper

--- Components/PlanSwiper/Presenter.js
+++ Components/PlanSwiper/Presenter.js
@@ -6,7 +6,7 @@ import {
 } from "react-native-iphone-x-helper";
 import styled from "styled-components";
 
-const { height } = Dimensions.get("window");
+const { height, width } = Dimensions.get("window");
 
 const Wrapper = styled.View`
 	width: 100%;
@@ -15,12 +15,21 @@ const Wrapper = styled.View`
 `;
 
 // 이게 swiper 혹은 scrollView가 돼야 한다.
-const SwiperWrapper = styled.View`
+const SwiperWrapper = styled.ScrollView.attrs({
+	horizontal: true,
+	pagingEnabled: true,
+	showsHorizontalScrollIndicator: false
+})`
 	width: 100%;
+	height: 100%;
+	flex-direction: row;
+`;
+
+const PlanBoxWrapper = styled.View`
+	width: ${width};
 	height: 100%;
 	align-items: center;
 `;
-
 //  이건 컴퍼넌트 분리 해야한다.
 const PlanBox = styled.SafeAreaView`
 	width: 95%;
@@ -28,15 +37,36 @@ const PlanBox = styled.SafeAreaView`
 	background-color: ${props => props.theme.planBoxColor};
 	border-bottom-left-radius: 50px;
 	border-bottom-right-radius: 50px;
+	box-shadow: 0 0 4px rgba(0, 0, 0, 0.3);
 `;
 const Text = styled.Text``;
 
 export default () => (
 	<Wrapper>
 		<SwiperWrapper>
-			<PlanBox>
-				<Text>hi</Text>
-			</PlanBox>
+			<PlanBoxWrapper>
+				<PlanBox>
+					<Text>hi</Text>
+				</PlanBox>
+			</PlanBoxWrapper>
+
+			<PlanBoxWrapper>
+				<PlanBox>
+					<Text>hi</Text>
+				</PlanBox>
+			</PlanBoxWrapper>
+
+			<PlanBoxWrapper>
+				<PlanBox>
+					<Text>hi</Text>
+				</PlanBox>
+			</PlanBoxWrapper>
+
+			<PlanBoxWrapper>
+				<PlanBox>
+					<Text>hi</Text>
+				</PlanBox>
+			</PlanBoxWrapper>
 		</SwiperWrapper>
 	</Wrapper>
 );
