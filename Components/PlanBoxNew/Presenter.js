ab7bace1745a3449dea0b3a82ea36cd7765a755e
jngcii <concotree@gmail.com>
2019-08-10 20:41:07 +0900

    Integrate ItemMakingBox to ItemActBox

--- Components/PlanBoxNew/Presenter.js
+++ Components/PlanBoxNew/Presenter.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import DraggableFlatList from "react-native-draggable-flatlist";
 import InputTitleText from "../InputTitleText";
 import InputItemActText from "../InputItemActText";
-import ItemMakingBox from "../ItemMakingBox";
+import ItemActBox from "../ItemActBox";
 
 const { width } = Dimensions.get("window");
 
@@ -144,8 +144,9 @@ export default ({
 					style={{ flex: 1, marginBottom: 20 }}
 					data={addedItem.array}
 					renderItem={({ index, item, isActive, move, moveEnd }) => (
-						<ItemMakingBox
+						<ItemActBox
 							item={item}
+							isEditing={true}
 							isActive={isActive}
 							// func
 							onRemoveItem={() => onRemoveItem(item)}
